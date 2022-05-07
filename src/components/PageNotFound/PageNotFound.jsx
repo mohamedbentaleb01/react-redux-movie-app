ffc11d4b3cb51c0088a9b771c14fd37a71fbc118
mohamedbentaleb01 <mohamedbentaleb007@gmail.com>
2022-05-07 23:29:04 +0000

    add page not found

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import './PageNotFound.scss';
+
+
 
 function PageNotFound() {
   return (
-    <div>PageNotFound</div>
+    <div>
+      <div className='Not-found-section'>
+        <img className='img-not-found' src='https://miro.medium.com/max/1400/1*DeBkx8vjbumpCO-ZkPE9Cw.png' alt='404' />
+      </div>
+    </div>
   )
 }
 
